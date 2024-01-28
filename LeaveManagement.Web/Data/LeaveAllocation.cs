ba68ba876ff9ff484bbf7c4f1e14ed99deb80bb1
Ragul <ragultamilmani823@gmail.com>
2024-01-28 17:51:42 +0530

    Add Database Tables and Migrations.

---
+++ LeaveManagement.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        
+        public int NumberofDays { get; set; }
+
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+
+        public int EmployeeId { get;  set; }
+
+
+    }
+}
