581d96be2e64a94fd31d1e59dc81d23a6a116ea5
Ragul <ragultamilmani823@gmail.com>
2024-01-30 19:08:58 +0530

    Performing curd operations and add some jquery for interactive delete operations

---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name ="Leave Type Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name="Default Number of days")]
+        [Required]
+        [Range(1,25,ErrorMessage ="Please Enter a valid Number")]
+        public int DefaultDays { get; set; }
+    }
+}
